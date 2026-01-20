bbdf99c3c0da411b8ae7f3d28c6558dc27d4ccba
jpyuan <yjpyuan@163.com>
2026-01-20 21:16:37 +0800

    task: 修改配置

--- engineering-docs-platform/docusaurus.config.ts
+++ engineering-docs-platform/docusaurus.config.ts
@@ -25,7 +25,7 @@ const config: Config = {
   organizationName: 'w3bank', // Usually your GitHub org/user name.
   projectName: 'w3bank.github.io', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -103,7 +103,7 @@ const config: Config = {
           items: [
             {
               label: 'Tutorial',
-              to: '/docs/intro',
+              to:'/docs/L0-enterprise/',
             },
           ],
         },
